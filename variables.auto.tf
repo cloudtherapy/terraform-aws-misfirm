49f767ead0eac0b1393876e6460b77e38201508e
Keith <laughmk@gmail.com>
2024-02-29 16:58:09 -0500

    initial push for misfirm aws buildout

--- variables.auto.tf
+++
@@ -1,7 +0,0 @@
-# AWS Network Infrastructure
-mis_access_key = ""
-mis_secret_key = ""
-aws_region       = "<aws_region>"
-#aws_vpc          = "<aws_vpc>"
-#aws_subnet_node1 = "<aws_subnet_node1>"
-#aws_subnet_node2 = "<aws_subnet_node2>"
