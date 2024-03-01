b716aeffd08f9aca1b92614b42fff2c79bf4c193
Keith <laughmk@gmail.com>
2024-03-01 16:17:20 -0500

    modifications for s3 bucket module

--- main.tf
+++ main.tf
@@ -7,17 +7,41 @@ terraform {
   }
 }
 
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.0"
+  bucket  = "mis-test-bucket1"
+  tags = {
+    "Environment" = "Prod"
+    "Owner"       = "Carlos"
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = "arn:aws:kms:us-east-1:255820308257:alias/aws/s3"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+#Importing s3 bucket
+resource "aws_s3_bucket" "mis-bucket2" {
+  bucket = "mis-test-bucket2"
+}
+
+#module "vpc" {
+#  source  = "terraform-aws-modules/vpc/aws"
+#  version = "5.5.2"
+#
+#}
+
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
   access_key = var.mis_access_key
   secret_key = var.mis_secret_key
-}
-
-resource "aws_s3_bucket" "mis-test" {
-  bucket = "misfirm-test-bucket"
-
-  tags = {
-    Environment = "Production"
-  }
 }
