ec3aaca49e2ad7582a910363b587d4ebae6d8d88
Keith <laughmk@gmail.com>
2024-03-04 11:01:05 -0500

    attempting 2 modules for 2 buckets

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-module "s3-bucket" {
+module "mis-test-bucket1" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.0"
   bucket  = "mis-test-bucket1"
@@ -26,12 +26,33 @@ module "s3-bucket" {
       }
     }
   }
+} 
+
+module "mis-test-bucket2" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.0"
+  bucket  = "mis-test-bucket2"
+  tags = {
+    "Environment" = "Dev"
+    "Owner"       = "Keith"
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = "arn:aws:kms:us-east-1:255820308257:alias/aws/s3"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 #Importing s3 bucket
-resource "aws_s3_bucket" "mis-bucket2" {
-  bucket = "mis-test-bucket2"
-}
+#resource "aws_s3_bucket" "mis-bucket2" {
+#  bucket = "mis-test-bucket2"
+#}
 
 #module "vpc" {
 #  source  = "terraform-aws-modules/vpc/aws"
